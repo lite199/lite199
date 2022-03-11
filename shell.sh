9b72c34d68b651f767e114f4f0cf2433ac0df981
hyppolite kamgaing <hyppolitekamgaing@hyppolites-MacBook-Pro.local>
2022-03-10 22:38:41 -0500

    new script

---
+++ shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+echo " Welcome to king forest: What is your age?"
+read age
+if [ $age -gt 12 ] 
+ then 
+    echo "enjoy "
+ else [ $age -lt 12 ] 
+ 
+     echo "Sorry you can come in "
+fi      
